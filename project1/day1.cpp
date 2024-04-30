bfd59c4931f2ae2d767c5ad89ab659c3e942eb90
雪豹 <386384641@qq.com>
2024-04-30 11:11:13 +0800

    这是我4月30日使用gitbash这个东东在clion上的修改

--- project1/day1.cpp
+++ project1/day1.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    cout << "hello world" << endl;
+    cout<<"hello"<<endl;
+    int a=8;
+    int b=4;
+    cout<<"你输入的a数字为\t"<<a<<"\t"<<"你输入的b数字为\t"<<b<<endl;
     return 0;
 }
