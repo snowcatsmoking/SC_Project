2515bc70ceaf9d1a6b8aeb375c3dcb0e5df128d8
gcaltair <143710288+gcaltair@users.noreply.github.com>
2024-04-30 15:29:41 +0800

    Update day1.cpp

--- project1/day1.cpp
+++ project1/day1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     //a simple test for different branches
-    int a=114;
+    int a=1;
     int b=514;
     cout<<a+b<<endl;
     cout<<"fetch successfully!"<<endl;
